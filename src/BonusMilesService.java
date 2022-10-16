7d235f6c7f0b5e071eec9cc897087a443006bee8
Georgii Pavlushchenko <8645141@gmail.com>
2022-10-16 16:01:02 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,18 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int price = 20;
+        int miles = cost/price;
+
+        return miles;
+
+
+    }
+
+
+
+
+
+
+    }
+
